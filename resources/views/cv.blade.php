98891fa906fecc9b7937251c353a783b4de17d08
emadelroby24 <emadelroby24@gmail.com>
2025-08-05 16:25:47 -0400

    Initiate Resume in JSON format

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CV</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>CV</h1>
+                    <p>This is a CV.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html> 
